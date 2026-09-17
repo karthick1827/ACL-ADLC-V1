d2fb4d71ca79e34a647536372f92c710940e128d
karthick1827 <nkarthick1827@gmail.com>
2026-09-17 13:59:07 +0530

    chore(release): bump version to 6.11.31

--- api/list-markdown-files.js
+++ api/list-markdown-files.js
@@ -246,8 +246,8 @@ module.exports = async function handler(req, res) {
             success: true,
             files: deduplicated,
             activeTier: activeTier,
-            frameworkVersion: '6.11.30',
-            version: '6.11.30',
+            frameworkVersion: '6.11.31',
+            version: '6.11.31',
             source: 'github',
             repo: `${owner}/${repo}`,
             branch: branch,
@@ -320,8 +320,8 @@ module.exports = async function handler(req, res) {
       success: true,
       files: finalDiskList,
       activeTier: diskActiveTier,
-      frameworkVersion: '6.11.30',
-      version: '6.11.30',
+      frameworkVersion: '6.11.31',
+      version: '6.11.31',
       source: 'local-disk',
     };
 
